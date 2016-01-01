659e42f98ccb79706acc5cc9f4cc080b8ff4dd6d
dassi <dassishenkar@users.noreply.github.com>
2016-01-01 21:55:11 +0200

    Initial commit

---
+++ app/src/main/java/com/tasking/tasking/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.tasking.tasking;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        EditText username  = (EditText)findViewById(R.id.txt_user_name);
+        EditText password  = (EditText)findViewById(R.id.txt_password);
+        TextView forgotMsg = (TextView)findViewById(R.id.txt_forgot);
+        TextView signUpMsg = (TextView)findViewById(R.id.txt_sign_msg);
+        Button button = (Button)findViewById(R.id.btn_sign);
+        Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/Raleway-Regular.ttf");
+        Typeface boldTypeFace = Typeface.createFromAsset(getAssets(), "fonts/Raleway-Bold.ttf");
+        username.setTypeface(typeFace);
+        password.setTypeface(typeFace);
+        forgotMsg.setTypeface(typeFace);
+        signUpMsg.setTypeface(typeFace);
+        button.setTypeface(boldTypeFace);
+        button.setTransformationMethod(null);
+    }
+}
---
+++ app/src/main/java/com/tasking/tasking/MainViewActivity.java
@@ -0,0 +1,14 @@
+package com.tasking.tasking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
