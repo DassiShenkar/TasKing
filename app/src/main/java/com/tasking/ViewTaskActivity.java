4bde367bc4c4c2ac3ae4ac34c92389502db9d10e
kogrego <kogrego@gmail.com>
2016-03-19 16:33:25 +0200

    added camera logic

--- app/src/main/java/com/tasking/ViewTaskActivity.java
+++ app/src/main/java/com/tasking/ViewTaskActivity.java
@@ -2,27 +2,18 @@ package com.tasking;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 public class ViewTaskActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 111;
-    static final int REQUEST_TAKE_PHOTO = 111;
-    String currentPhotoPath;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,70 +25,25 @@ public class ViewTaskActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Bundle userParams = getIntent().getExtras();
         Task task = TaskDAO.getInstance(this).getTask(userParams.getInt("taskId"));
-        ImageButton imageButton = (ImageButton) findViewById(R.id.btn_img_save);
-        //TODO: fix data returns null
-        if (data != null && requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        ImageView imageview = (ImageView) findViewById(R.id.btn_img_save);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageButton.setImageBitmap(imageBitmap);
-            imageButton.setClickable(false);
+            imageview.setImageBitmap(imageBitmap);
             ByteArrayOutputStream bYtE = new ByteArrayOutputStream();
-            if (imageBitmap != null){
-                imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, bYtE);
-                imageBitmap.recycle();
-            }
             byte[] byteArray = bYtE.toByteArray();
             String imageFile = Base64.encodeToString(byteArray, Base64.DEFAULT);
             task.setPicture(imageFile);
             TaskDAO.getInstance(this).updateTask(task);
-            galleryAddPic();
+            imageview.setClickable(false);
         }
     }
 
     public void takePhoto(View view){
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-                //...
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-                        Uri.fromFile(photoFile));
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
-    }
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = "file:" + image.getAbsolutePath();
-        return image;
-    }
-
-    private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(currentPhotoPath);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        this.sendBroadcast(mediaScanIntent);
     }
 
     public void done(View view){
