688a2d76cbd5cde6d75aa32560652913e34dfc67
hamsterduck <dassi.rosen@gmail.com>
2016-03-20 01:21:26 +0200

    sign up and login update

--- app/src/main/java/com/tasking/LoginActivity.java
+++ app/src/main/java/com/tasking/LoginActivity.java
@@ -21,10 +21,7 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
-import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Map;
 
 
@@ -33,41 +30,8 @@ public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-        //  Gindos DB CHECK - DO NOT TOUCH
-        Firebase.setAndroidContext(this);
-        FireBaseDB db = FireBaseDB.getInstance();
-
-
-        Manager manager = new Manager("manager17@gmail.com", "15243w");
-        Firebase ref = new Firebase("https://tasking-android.firebaseio.com/");
-//        manager.setUid(ref.child("users").child("uid").getKey().toString());
-
-      /*  Map users = (Map<String, String>)ref.child("users");
-        for(Object key :
-            users.keySet()){
-                System.out.println(key.toString());
-        }*/
-        System.out.println("uid=" + manager.getUid());
-        TeamMember e4 = new TeamMember("arel10@gmail.com", "1232214w345");
-        TeamMember e5 = new TeamMember("arel11@gmail.com", "12322454514w345");
-        TeamMember e6 = new TeamMember("arel12@gmail.com", "12322454514ew345");
-        //  db.createNewEmployee(e);
-//        Task task = new Task("Endroid1","11/09/1990","homework","urgent","room 247","waiting",e6);
-        // String mangerUid = db.createNewManager("new12anager@gmail.com","15243w");
-        // AuthData ad = db.ge
-//        db.createNewManager(manager);
-//        db.authenticationEmployee(manager);
-        System.out.println("login=" + db.getManagerUid());
-        // db.addNewTask(task);
-        //  db.setTaskAssignees(task,e4);
-        //  db.setManagerEmployees(e4,teamMember);
-
-
-
-
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(com.tasking.R.layout.activity_login);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
@@ -109,33 +73,31 @@ public class LoginActivity extends Activity {
     }
 
     public void submit(View view) {
+        Firebase firebase = new Firebase("https://tasking-android.firebaseio.com/");
         Button signUp = (Button) findViewById(R.id.btn_sign);
-        EditText username = (EditText) findViewById(R.id.txt_user_name);
-        EditText password = (EditText) findViewById(R.id.txt_password);
-        String user = username.getText().toString();
-        String pass = password.getText().toString();
-        Bundle userParams = getIntent().getExtras();
+        EditText editUsername = (EditText) findViewById(R.id.txt_user_name);
+        EditText editPassword = (EditText) findViewById(R.id.txt_password);
+        String username = editUsername.getText().toString();
+        String password = editPassword.getText().toString();
+        //sign up (managers only)
         if (signUp.getText().toString().equals((getResources().getString(R.string.sign_up)))) {
-            if (!user.equals("")) {
-                if (!pass.equals("")) {
-                    final Firebase ref = new Firebase("https://tasking-android.firebaseio.com/");
-                    ref.createUser(user, pass, new Firebase.ValueResultHandler<Map<String, Object>>() {
+            if (!username.equals("")) {
+                if (!password.equals("")) {
+                    firebase.createUser(username, password, new Firebase.ValueResultHandler<Map<String, Object>>() {
                         @Override
                         public void onSuccess(Map<String, Object> result) {
                             String uid = result.get("uid").toString();
                             Bundle userParams = getIntent().getExtras();
                             userParams.putString("uid", uid);
+                            userParams.putBoolean("isManager", true); // only managers can sign up
                             Intent intent = new Intent(getApplication(), TeamActivity.class);
-                            userParams.putBoolean("isManager", true); // after we check if isManager
                             intent.putExtras(userParams);
                             startActivity(intent);
-                            System.out.println("Successfully created user account with uid: " + result.get("uid"));
                         }
 
                         @Override
                         public void onError(FirebaseError firebaseError) {
-                            System.out.println("error on create manager");
-                            // there was an error
+                            //TODO: something
                         }
                     });
                 } else {
@@ -145,60 +107,56 @@ public class LoginActivity extends Activity {
                 Toast.makeText(this, "Please enter user name", Toast.LENGTH_SHORT).show();
             }
         } else {
-            if (!user.equals("")) {
-                if (!pass.equals("")) {
-                    final Firebase ref = new Firebase("https://tasking-android.firebaseio.com/");
-                    ref.authWithPassword(user, pass,
+            //login
+            if (!username.equals("")) {
+                if (!password.equals("")) {
+                    firebase.authWithPassword(username, password,
                             new Firebase.AuthResultHandler() {
                                 @Override
                                 public void onAuthenticated(AuthData authData) {
-                                    // save bullean auth true to shared prefference;
-
+                                    final String uid = authData.getUid();
+                                    Firebase firebase = new Firebase("https://tasking-android.firebaseio.com/");
                                     SharedPreferences settings = getSharedPreferences("user_pref", MODE_PRIVATE);
                                     SharedPreferences.Editor prefEditor = settings.edit();
                                     prefEditor.putBoolean("isAuthenticated", true);
                                     prefEditor.apply();
-
-                                    final String uid = authData.getUid();
                                     if (uid != null) {
-                                        ref.addValueEventListener(new ValueEventListener() {
+                                        firebase.addValueEventListener(new ValueEventListener() {
                                             @Override
                                             public void onDataChange(DataSnapshot snapshot) {
                                                 Bundle userParams = getIntent().getExtras();
+                                                //Manager login
                                                 if (snapshot.child("Managers").child(uid).getKey().equals(uid)) {
-                                                    if (snapshot.child("Managers").child("Team").getKey() != null) {
-                                                        Intent intent = new Intent(getApplication(), TasksActivity.class);
+                                                    //Manager with team
+                                                    if (snapshot.child("Managers").child(uid).getChildrenCount() > 1) {
                                                         userParams.putString("uid", uid);
                                                         userParams.putBoolean("isManager", true);
+                                                        //TODO: save team in local db
+                                                        Intent intent = new Intent(getApplication(), TasksActivity.class);
                                                         intent.putExtras(userParams);
                                                         startActivity(intent);
-                                                        JSONObject teamMemebers = (JSONObject)snapshot.child("Managers").child("Team").getValue();
-                                                        Iterator<String> keys = teamMemebers.keys();
-                                                        while (keys.hasNext()) {
-
-                                                        }
-                                                        //save localy users + tasks
+                                                    //Manager with no team
                                                     } else {
-                                                        Intent intent = new Intent(getApplication(), TeamActivity.class);
                                                         userParams.putString("uid", uid);
                                                         userParams.putBoolean("isManager", true);
+                                                        Intent intent = new Intent(getApplication(), TeamActivity.class);
                                                         intent.putExtras(userParams);
                                                         startActivity(intent);
                                                     }
+                                                // Team Member
                                                 } else {
-                                                    Intent intent = new Intent(getApplication(), TasksActivity.class);
                                                     userParams.putString("uid", uid);
                                                     userParams.putBoolean("isManager", false);
+                                                    //TODO: save tasks in local db
+                                                    Intent intent = new Intent(getApplication(), TasksActivity.class);
                                                     intent.putExtras(userParams);
                                                     startActivity(intent);
-                                                    //save localy tasks
                                                 }
                                             }
 
                                             @Override
                                             public void onCancelled(FirebaseError firebaseError) {
                                                 Toast.makeText(getApplication(), firebaseError.getMessage(), Toast.LENGTH_SHORT).show();
-
                                             }
                                         });
                                     }
@@ -206,13 +164,12 @@ public class LoginActivity extends Activity {
 
                                 @Override
                                 public void onAuthenticationError(FirebaseError error) {
-                                    //get authenticated localy if(auth) if has team localy goto task else goto team
                                     SharedPreferences preferences = getPreferences(MODE_PRIVATE);
                                     boolean isAuthenticated = preferences.getBoolean("isAuthenticated", true);
                                     if(isAuthenticated) {
                                         Bundle userParams = getIntent().getExtras();
-                                        ArrayList<Employee> teamMembres = TaskDAO.getInstance(getApplication()).getMembers();
-                                        boolean isManager = teamMembres.size() > 0;
+                                        ArrayList<Employee> teamMembers = TaskDAO.getInstance(getApplication()).getMembers();
+                                        boolean isManager = teamMembers.size() > 0;
                                         userParams.putBoolean("isManager", isManager);
                                         Intent intent = new Intent(getApplication(), TasksActivity.class);
                                         userParams.putString("workMode", "offline");
