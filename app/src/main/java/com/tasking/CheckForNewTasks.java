325e10787c6bbb3fa30209df9a19bbd08556cb46
Arel Gindos <rlgindos@gmail.com>
2016-04-02 04:47:34 +0300

    settings activity

---
+++ app/src/main/java/com/tasking/CheckForNewTasks.java
@@ -0,0 +1,78 @@
+package com.tasking;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Arel on 02/04/2016.
+ */
+public class CheckForNewTasks {
+
+    private static CheckForNewTasks instance = null;
+    private int time;
+    private NumberPicker np;
+    private Timer timer;
+    private Context context;
+    private Bundle userParams;
+
+    public static CheckForNewTasks getInstance(Context context, Bundle userParams) {
+        if (instance == null) {
+            instance = new CheckForNewTasks(context, userParams);
+        }
+        return instance;
+    }
+
+    public CheckForNewTasks(Context context, Bundle userParams) {
+        this.context = context;
+        this.userParams = userParams;
+        this.time = 5;
+        timer = new Timer();
+        startSchedule(time);
+    }
+
+    public void stopSchedule() {
+        timer.cancel();
+        timer.purge();
+    }
+
+    public void startSchedule(int time) {
+        timer.schedule(checkNewTasks, 1, 1000 * 60 * time);
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    TimerTask checkNewTasks = new TimerTask() {
+        @Override
+        public void run() {
+            Firebase firebase = new Firebase("https://tasking-android.firebaseio.com/");
+            firebase.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    new AsyncUpdateTasks(context, userParams, snapshot).execute();
+                }
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+                    Toast.makeText(context, firebaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    };
+}
+
--- app/src/main/java/com/tasking/SettingsActivity.java
+++ app/src/main/java/com/tasking/SettingsActivity.java
@@ -1,14 +1,41 @@
 package com.tasking;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    private  NumberPicker np;
+    private CheckForNewTasks checkForNewTasks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        //TODO: after backend support
+        Bundle userParams = getIntent().getExtras();
+        checkForNewTasks = CheckForNewTasks.getInstance(SettingsActivity.this,userParams);
+        np = (NumberPicker) findViewById(R.id.number_curr_interval);
+        np.setMinValue(5);
+        np.setMaxValue(60);
+        np.setValue(checkForNewTasks.getTime());
+        np.setWrapSelectorWheel(false);
+    }
+    public void save(View view){
+        if(np.getValue() != checkForNewTasks.getTime() ){ // time interval in setting changed
+            checkForNewTasks.setTime(np.getValue());
+            checkForNewTasks.stopSchedule();
+            checkForNewTasks.startSchedule(np.getValue());
+        }
+        Toast.makeText(getApplication(), "Check for new Tasks every "+np.getValue()+" minutes.", Toast.LENGTH_SHORT).show();
+        Bundle userParams = getIntent().getExtras();
+        Intent intent = new Intent(this, TasksActivity.class);
+        intent.putExtras(userParams);
+        startActivity(intent);
     }
 }
