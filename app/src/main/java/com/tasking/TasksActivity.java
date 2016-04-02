0b57b1eebd29b4ae473e8059b0ed1b77fbb33948
kogrego <kogrego@gmail.com>
2016-04-02 06:01:42 +0300

    added ShowImageActivity

--- app/src/main/java/com/tasking/TasksActivity.java
+++ app/src/main/java/com/tasking/TasksActivity.java
@@ -113,7 +113,7 @@ public class TasksActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+        drawer.setDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
         createTask.setTypeface(typeFace);
