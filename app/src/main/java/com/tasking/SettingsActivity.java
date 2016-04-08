353bf7dd555b357a95f1073774751de87f82afd6
Gregory <grisha_kon@hotmail.com>
2016-04-08 03:07:17 +0300

    qqq

--- app/src/main/java/com/tasking/SettingsActivity.java
+++ app/src/main/java/com/tasking/SettingsActivity.java
@@ -22,17 +22,18 @@ public class SettingsActivity extends AppCompatActivity {
 
     public void save(View view){
         EditText np = (EditText) findViewById(R.id.number_curr_interval);
-
-        int picked = Integer.valueOf(np.getText().toString());
-        if(picked != checkForNewTasks.getTime() ){ // time interval in setting changed
-            checkForNewTasks.setTime(picked);
-            checkForNewTasks.stopSchedule();
-            checkForNewTasks.startSchedule(picked);
+        if(np.getText() != null) {
+            int picked = Integer.valueOf(np.getText().toString());
+            if (picked != checkForNewTasks.getTime()) { // time interval in setting changed
+                checkForNewTasks.setTime(picked);
+                checkForNewTasks.stopSchedule();
+                checkForNewTasks.startSchedule(picked);
+            }
+            Toast.makeText(getApplication(), "Check for new Tasks every " + picked + " minutes.", Toast.LENGTH_SHORT).show();
+            Bundle userParams = getIntent().getExtras();
+            Intent intent = new Intent(this, TasksActivity.class);
+            intent.putExtras(userParams);
+            startActivity(intent);
         }
-        Toast.makeText(getApplication(), "Check for new Tasks every "+picked+" minutes.", Toast.LENGTH_SHORT).show();
-        Bundle userParams = getIntent().getExtras();
-        Intent intent = new Intent(this, TasksActivity.class);
-        intent.putExtras(userParams);
-        startActivity(intent);
     }
 }
